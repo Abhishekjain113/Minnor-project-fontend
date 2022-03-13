c9b0077897f7aebef561dcabe0de84408fec7e09
jabhishek <jainabhishek1118@gmail.com>
2022-03-13 09:45:52 +0530

    Signin/Signup 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+//react router dom
+import { Routes, Route,  } from "react-router-dom";
+
+
+
+
+//import scrrens
+import Login from './screen/Login';
+import SignIn from './screen/Signin'
+import Forgotpassword from './screen/Forgot-Password';
+import OTP from './screen/Otp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+  <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="Signup" element={<SignIn />} />
+        <Route path="forgotpassword" element={<Forgotpassword />} />
+        <Route path="OTP" element={<OTP />} />
+
+      </Routes>
+  </>  );
 }
 
 export default App;
